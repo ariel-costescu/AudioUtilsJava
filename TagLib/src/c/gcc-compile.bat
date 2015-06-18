b6bd9ecc66b98c6b0b5a65fa1c5821868c77ef4d
ariel-costescu <black.t.ro@gmail.com>
2015-06-18 19:20:51 +0300

    Remove old project folder

--- TagLib/src/c/gcc-compile.bat
+++
@@ -1,1 +0,0 @@
-gcc -Wl,--add-stdcall-alias -I"%JAVA_HOME%\include" -I"%JAVA_HOME%\include\win32" -I"C:\chromaprint\include" -I"C:\ffmpeg\include" -L"C:\chromaprint\lib" -L"C:\ffmpeg\lib" -shared -o ChromaPrint.dll ChromaPrint.c -llibavcodec -llibavdevice -llibavfilter -llibavformat -llibavutil -llibpostproc -llibswresample -llibswscale -llibchromaprint
